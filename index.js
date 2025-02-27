c626a43ea5a06433b64fc0e8c183f53aebf14923
Sharon Kendi <sharonkendi35@gmail.com>
2025-02-27 16:50:52 +0300

    Updats and deletes employee records

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sam",
+    streetAddress: "11 Broadway",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee }; // Create a shallow copy
+    delete newEmployee[key]; // Delete key from the new copy
+    return newEmployee; // Return the new copy, original remains unchanged
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; // Directly modify the original object
+    return employee;
+  }
+  
